dc610434271ac3dde3eecd22efa7dd5eb5c1a7af
iqandiiman <iqandiiman1910@gmail.com>
2022-07-21 09:26:06 +0700

    Tugas UAS PEMROGRAMAN WEB 2

---
+++ adminpanel/logout.php
@@ -0,0 +1,6 @@
+<?php
+require 'login.php';
+session_start();
+session_destroy();
+?>
+
---
+++ tentang-kami.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Online | Tentang Kami</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php require "navbar.php"; ?>
+
+    <!-- banner -->
+    <div class="container-fluid banner2 d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">Tentang Kami</h1>
+        </div>
+    </div>
+
+    <!-- main -->
+    <div class="container-fluid py-5">
+        <div class="container fs-5 text-center">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur sed aspernatur optio non dolorem atque quidem corrupti fuga, officia voluptatem. Dolore architecto doloremque iusto nemo ea corrupti soluta ducimus, molestiae ratione! Deserunt quisquam blanditiis, quidem minus, animi molestias beatae ea voluptatibus tempore modi dolore in consequuntur quaerat omnis commodi a assumenda quo aliquid veritatis at! Animi veritatis debitis, adipisci expedita at libero beatae, labore delectus, perferendis nam reiciendis? Unde assumenda eum numquam deleniti accusantium culpa recusandae cum dicta quidem eius laborum, qui similique. Reprehenderit provident veniam accusamus odio, repudiandae reiciendis inventore corporis dolore nostrum! Nobis modi pariatur repellat sunt aut?</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi suscipit eaque reprehenderit id enim dolor sunt itaque et accusantium odit vitae illum quo vel nostrum, quaerat esse voluptatem ipsum placeat! Deserunt amet recusandae odio labore neque consectetur corrupti necessitatibus officia rerum atque, perspiciatis ipsa, dolor ut, veniam praesentium fugit alias? Distinctio, qui. Autem laboriosam excepturi impedit. Officiis voluptatum sint, id laborum maxime excepturi commodi fugiat error expedita provident doloremque, cumque nam repudiandae ut deleniti quaerat nulla reprehenderit, aliquam nihil temporibus. Consectetur deleniti ex nulla nemo dolorum? Quos consequatur quae blanditiis, eligendi sit quasi repudiandae tempore itaque exercitationem rem facilis quis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis odio odit amet voluptate saepe doloribus hic, debitis unde error vero molestiae numquam tempora? Natus molestias quidem incidunt in, praesentium perferendis atque voluptatum similique ullam nisi voluptatem quis, velit consequuntur quas officia deleniti ducimus laboriosam debitis provident vitae recusandae tempore omnis? Consequuntur hic aut voluptatem accusantium doloribus veritatis alias facere eos nihil illo minima aliquam nobis, harum eligendi debitis illum ea facilis. Sit, earum. Magnam, facilis? In molestiae obcaecati sapiente ea ipsa, voluptatum debitis! Molestias laudantium provident modi consectetur incidunt rem tenetur, commodi reprehenderit beatae distinctio minus, culpa enim atque consequuntur.</p>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <?php require "footer.php"; ?>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+</body>
+
+</html>
